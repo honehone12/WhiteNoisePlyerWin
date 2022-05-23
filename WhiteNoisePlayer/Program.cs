1573c4f6a447cd90a52d49a984149483eb9f298c
honehone12 <54838229+honehone12@users.noreply.github.com>
2022-05-23 19:19:14 +0900

    initial commit

---
+++ WhiteNoisePlayer/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Threading;
+using CSCore;
+using CSCore.Codecs;
+using CSCore.CoreAudioAPI;
+using CSCore.SoundOut;
+
+namespace WhiteNoisePlayer
+{
+    internal class Program
+    {
+        //set right path when release
+        const string fileName = "noise.wav";
+
+        static void Main()
+        {
+            IWaveSource waveSource;
+            try
+            {
+                waveSource = CodecFactory.Instance.GetCodec(fileName)
+                .ToMono()
+                .Loop();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please make sure noise.wav is in same folder.");
+                // to show error message
+                while (true)
+                {
+                    Thread.Sleep(100);
+                }
+            }
+            
+            MMDevice device;
+            using (var mmDeviceEnumerator = new MMDeviceEnumerator())
+            {
+                device = mmDeviceEnumerator.GetDefaultAudioEndpoint(
+                    DataFlow.Render,
+                    Role.Multimedia
+                );
+            }
+            var soundOut = new WasapiOut()
+            {
+                Latency = 100,
+                Device = device
+            };
+
+            soundOut.Initialize(waveSource);
+            soundOut.Play();
+            double currentVolume = soundOut.Volume;
+
+            Console.WriteLine("Welcome !! This is WhiteNoisePlayer !!");
+            Console.WriteLine("Press ...");
+            Console.WriteLine("[q] to quit.");
+            Console.WriteLine("[w] to up volume");
+            Console.WriteLine("[s] to down volume");
+
+            var appLoop = true;
+            while (appLoop)
+            {
+                Console.WriteLine($"Current volume: {currentVolume} (0-1)");
+                var ctl = Console.ReadLine();
+                switch (ctl)
+                {
+                    case "q":
+                        appLoop = false;
+                        break;
+                    case "w":
+                        if(currentVolume < 1.0)
+                        {
+                            currentVolume += 0.1;
+                            if(currentVolume > 1.0)
+                            {
+                                currentVolume = 1.0;
+                            }
+                            soundOut.Volume = (float)currentVolume;
+                        }
+                        break;
+                    case "s":
+                        if (currentVolume > 0.0)
+                        {
+                            currentVolume -= 0.1;
+                            if (currentVolume < 0.0)
+                            {
+                                currentVolume = 0.0;
+                            }
+                            soundOut.Volume = (float)currentVolume;
+                        }
+                        break;
+                }
+
+                Thread.Sleep(500);
+            }
+
+            soundOut.Stop();
+            soundOut.Dispose(); 
+            Console.WriteLine("Thank you !! Byebye !!");
+        }
+    }
+}
---
+++ WhiteNoisePlayerGUI/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Windows;
+using CSCore;
+using CSCore.Codecs;
+using CSCore.CoreAudioAPI;
+using CSCore.SoundOut;
+
+namespace WhiteNoisePlayerGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //set right path when release
+        const string fileName = "noise.wav";
+
+        readonly WasapiOut soundOut;
+
+        public MainWindow()
+        {
+            IWaveSource waveSource;
+            try
+            {
+                waveSource = CodecFactory.Instance.GetCodec(fileName)
+                .ToMono()
+                .Loop();
+            }
+            catch (Exception e)
+            {
+                // better way to show error message ??
+                Console.WriteLine("Please make sure noise.wav is in same folder.");
+                throw e;
+            }
+
+            MMDevice device;
+            using (var mmDeviceEnumerator = new MMDeviceEnumerator())
+            {
+                device = mmDeviceEnumerator.GetDefaultAudioEndpoint(
+                    DataFlow.Render,
+                    Role.Multimedia
+                );
+            }
+            
+            soundOut = new WasapiOut()
+            {
+                Latency = 100,
+                Device = device
+            };
+
+            soundOut.Initialize(waveSource);
+            soundOut.Play();
+
+            InitializeComponent();
+        }
+
+        private void VolumeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            soundOut.Volume = (float)e.NewValue;
+        }
+
+        private void QuitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        
+        private void PlayerWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            soundOut.Stop();
+            soundOut.Dispose();
+        }
+    }
+}
